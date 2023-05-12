075d7897d603297e448878e0dd1238f68a1b8b59
fedemoretto11 <fedemoretto94@gmail.com>
2023-05-11 23:49:56 -0300

    Primera prueba de landing page

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const burguerMenu = document.getElementById("burguer-menu");
+const menuDesplegable = document.querySelector(".sidebar");
+
+
+
+
+function desplegarMenu() {
+
+  menuDesplegable.style.right = "0px";
+  document.querySelector(".line1").classList.add("line1-active");
+  document.querySelector(".line2").classList.add("line2-active");
+  document.querySelector(".line3").classList.add("line3-active");
+
+}
+
+function esconderMenu() {
+  menuDesplegable.style.right = "-10000px";
+  document.querySelector(".line1").classList.remove("line1-active");
+  document.querySelector(".line2").classList.remove("line2-active");
+  document.querySelector(".line3").classList.remove("line3-active");
+}
+
+
+
+
+
+burguerMenu.addEventListener("click", (e) => {
+  if (menuDesplegable.style.right == "-10000px") {
+    desplegarMenu();
+  } else {
+    esconderMenu();
+  }
+})
