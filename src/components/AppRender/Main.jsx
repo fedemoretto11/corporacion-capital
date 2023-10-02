0831ca82ecb551f2fb50e628ef4cff4ce4d27197
fedemoretto11 <fedemoretto94@gmail.com>
2023-10-02 10:01:46 -0300

    cambios en release, todavia noe sta listo

--- src/components/AppRender/Main.jsx
+++ src/components/AppRender/Main.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom"
 import Index from "../Pages/Index"
-import Nosotros from "../Pages/Nosotros"
 import EmpleosPage from "../Pages/EmpleosPage"
 import AplicarContainer from "../Aplicar/AplicarContainer"
 import Jobs from "../../db/jobs"
@@ -12,7 +11,6 @@ function Main() {
     <main id="main" className="flex">
       <Routes>
         <Route path="/" element={<Index />}></Route>
-        <Route path="/nosotros" element={<Nosotros />}></Route>
         <Route path="/empleos" element={<EmpleosPage />}></Route>
         <Route path="/empleos/aplicar/:id" element={<AplicarContainer />}></Route>
         <Route path="/comunidad" element={<Jobs />}></Route>
